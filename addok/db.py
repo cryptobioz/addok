67690869a9108583b6aa58b233bb48347cfa035a
Léo Depriester <leo.depriester@camptocamp.com>
2020-08-26 12:23:29 +0200

    db: add metrics function

--- addok/db.py
+++ addok/db.py
@@ -1,8 +1,8 @@
 import redis
 from hashids import Hashids
+from prometheus_client import Gauge
 
 from addok.config import config
-
 
 hashids = Hashids()
 
@@ -13,6 +13,9 @@ class RedisProxy:
 
     def connect(self, *args, **kwargs):
         self.instance = redis.StrictRedis(*args, **kwargs)
+        self.metrics_catalog = {
+            "redis_key_count": Gauge('addok_redis_keys_count', 'Count of Redis keys'),
+        }
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
@@ -20,6 +23,10 @@ class RedisProxy:
     def next_id(self):
         next_id = self.incr('_id_sequence')
         return hashids.encode(next_id)
+
+    def metrics(self):
+        keys_count = self.instance.dbsize()
+        self.metrics_catalog["redis_key_count"].set(keys_count)
 
 
 DB = RedisProxy()
